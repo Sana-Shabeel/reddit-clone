6c4afb2197fb7d2411212e5652d341070b047ff0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-09 21:56:34 +0300

    recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
 import type { AppProps } from "next/app";
 import Layout from "@/componenets/Layout/Layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
