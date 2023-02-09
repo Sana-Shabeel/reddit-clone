2c6699974c3b46c34b85c7dca83574504622bf1b
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-09 21:58:46 +0300

    Navbar

--- src/componenets/Navbar/Navbar.tsx
+++ src/componenets/Navbar/Navbar.tsx
@@ -1,5 +1,23 @@
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
+import AuthButtons from "./RightContent/AuthButtons";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" alt="reddit logo" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          alt="reddit name"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      <SearchInput />
+      <RightContent />
+    </Flex>
+  );
 };
