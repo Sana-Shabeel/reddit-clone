00b3d26907deea0e82c6a043e984cde21e0d8e4a
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-10 07:49:09 +0300

    Auth Inputs

--- src/componenets/Modal/Auth/AuthInputs.tsx
+++ src/componenets/Modal/Auth/AuthInputs.tsx
@@ -3,13 +3,14 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilValue } from "recoil";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
 const AuthInputs = () => {
   const modalState = useRecoilValue(authModalState);
   return (
-    <Flex>
+    <Flex mt="4">
       {modalState.view === "login" && <Login />}
-      {/* {modalState.view === "signup" && <SignUp />} */}
+      {modalState.view === "signup" && <SignUp />}
     </Flex>
   );
 };
