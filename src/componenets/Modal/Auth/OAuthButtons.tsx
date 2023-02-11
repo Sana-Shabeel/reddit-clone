b89414d969d4146f6c7eee753cd942af93ee73fe
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-11 08:52:23 +0300

    Add sign in with google logic

--- src/componenets/Modal/Auth/OAuthButtons.tsx
+++ src/componenets/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,17 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import React from "react";
+import { auth } from "@/firebase/clientApp";
 
 const OAuthButtons = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction="column" mb={4} width="100%">
       <Button
         variant="oauth"
         mb={2}
-        // onClick={() => signInWithGoogle()}
-        // isLoading={loading}
+        onClick={() => signInWithGoogle()}
+        isLoading={loading}
       >
         <Image
           src="/images/googlelogo.png"
@@ -19,11 +22,11 @@ const OAuthButtons = () => {
         Continue with Google
       </Button>
       <Button variant="oauth">Some Other Provider</Button>
-      {/* {error && (
+      {error && (
         <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
-          {error}
+          {error.message}
         </Text>
-      )} */}
+      )}
     </Flex>
   );
 };
