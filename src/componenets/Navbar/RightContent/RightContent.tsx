40f2cb1285e892b3d3e37a64c35cb257cc891132
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-11 08:55:17 +0300

    Add sign out logic

--- src/componenets/Navbar/RightContent/RightContent.tsx
+++ src/componenets/Navbar/RightContent/RightContent.tsx
@@ -1,14 +1,24 @@
 import AuthModal from "@/componenets/Modal/Auth/AuthModal";
-import { Flex } from "@chakra-ui/react";
+import { auth } from "@/firebase/clientApp";
+import { Button, Flex } from "@chakra-ui/react";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import AuthButtons from "./AuthButtons";
 
-const RightContent = () => {
+interface Props {
+  user: User | null | undefined;
+}
+
+const RightContent = ({ user }: Props) => {
   return (
     <>
       <Flex justify={"center"} align="center">
         <AuthModal />
-        <AuthButtons />
+        {user ? (
+          <Button onClick={() => signOut(auth)}>Sign Out</Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
